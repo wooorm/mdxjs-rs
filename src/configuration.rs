766541860b7417a083602fc423ca4ebfb9d30082
Titus Wormer <tituswormer@gmail.com>
2022-10-20 14:10:01 +0200

    Remove commented-out code for coverage

--- src/configuration.rs
+++ src/configuration.rs
@@ -152,91 +152,12 @@ impl MdxParseOptions {
     }
 }
 
-// To do: use `Format`.
-// /// Format the file is in (default: `Format::Detect`).
-// #[derive(Clone, Debug, Default, Eq, PartialEq)]
-// pub enum Format {
-//     /// Use `Format::Markdown` for files with an extension in `md_extensions`
-//     /// and `Format::Mdx` otherwise.
-//     #[default]
-//     Detect,
-//     /// Treat file as MDX.
-//     Mdx,
-//     /// Treat file as plain vanilla markdown.
-//     Markdown,
-// }
-
-// To do: use `OutputFormat`.
-// /// Output format to generate (default: `OutputFormat::Program`).
-// #[derive(Clone, Debug, Default, Eq, PartialEq)]
-// pub enum OutputFormat {
-//     /// The `Program` format will use import statements to import the JSX
-//     /// runtime (and optionally provider) and use an export statement to yield
-//     /// the `MDXContent` component.
-//     #[default]
-//     Program,
-//     /// The `FunctionBody` format will get the JSX runtime (and optionally
-//     /// provider) from `arguments[0]`, rewrite export statements, and use a
-//     /// return statement to yield what was exported.
-//     /// Normally, this output format will throw on `import` (and
-//     /// `export … from`) statements, but you can support them by setting
-//     /// `options.useDynamicImport`.
-//     FunctionBody,
-// }
-
 /// Configuration (optional).
 #[derive(Clone, Debug)]
 pub struct Options {
     /// Configuration that describes how to parse from markdown.
     pub parse: MdxParseOptions,
 
-    // /// List of markdown extensions, with dot.
-    // ///
-    // /// Default: `vec![".md".into(), ".markdown".into(), ".mdown".into(), ".mkdn".into(), ".mkd".into(), ".mdwn".into(), ".mkdown".into(), ".ron".into()]`.
-    // pub md_extensions: Option<Vec<String>>,
-    // /// List of MDX extensions, with dot.
-    // ///
-    // /// Default: `vec![".mdx".into()]`.
-    // pub mdx_extensions: Option<Vec<String>>,
-    // /// Format the file is in (default: `Format::Detect`).
-    // pub format: Format,
-    // /// To do: support `output_format: FunctionBody
-    // /// Output format to generate (default: `OutputFormat::Program`).
-    // ///
-    // /// In most cases `OutputFormat::Program` should be used, as it results in a
-    // /// whole program.
-    // /// `OutputFormat::FunctionBody` can be used to compile to code that can be
-    // /// `eval`ed.
-    // /// In some cases, you might want to do that, such as when compiling on the
-    // /// server and running on the client.
-    // pub output_format: OutputFormat,
-    // /// Whether to compile to dynamic import expressions (default:
-    // /// `false`).
-    // ///
-    // /// This option applies when `options.output_format` is
-    // /// `OutputFormat::FunctionBody`.
-    // ///
-    // /// This project can turn import statements (`import x from 'y'`) into
-    // /// dynamic imports (`const {x} = await import('y')`).
-    // /// This is useful because import statements only work at the top level of
-    // /// JavaScript modules, whereas `import()` is available inside function
-    // /// bodies.
-    // ///
-    // /// When you turn `use_dynamic_import` on, you should probably set
-    // /// `options.base_url` too.
-    // pub use_dynamic_import: bool,
-    // /// Resolve `import`s (and `export … from`, `import.meta.url`) from this
-    // /// URL (default: `None`, example: `Some("https://example.com/".into())`).
-    // ///
-    // /// Relative specifiers are non-absolute URLs that start with `/`, `./`, or
-    // /// `../`.
-    // /// For example: `/index.js`, `./folder/file.js`, or `../main.js`.
-    // ///
-    // /// This option is useful when code will run in a different place.
-    // /// One example is when `.mdx` files are in path *a* but compiled to path
-    // /// *b* and imports should run relative the path *b*.
-    // /// Another example is when evaluating code, whether in Node or a browser.
-    // pub base_url: Option<String>,
     /// Whether to add extra information to error messages in generated code
     /// (default: `false`).
     ///
